ed39c2365f5b1060f5a4e34b94c2b06ab49a23ef
mstaali <imthetigerz@gmail.com>
2023-12-03 19:12:17 +0100

    proto_modif

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 12:53:22 by mstaali           #+#    #+#             */
-/*   Updated: 2023/12/03 15:16:37 by mstaali          ###   ########.fr       */
+/*   Updated: 2023/12/03 18:53:40 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_putstr(char *s);
 int	ft_putnbr(int d);
 int	ft_putnbr_unsigned(unsigned int u);
 int	ft_putptr_hexa(uintptr_t p);
-int	ft_putnbr_hexalower(int x);
-int	ft_putnbr_hexaupper(int x);
+int	ft_putnbr_hexalower(unsigned int x);
+int	ft_putnbr_hexaupper(unsigned int x);
 
 #endif
