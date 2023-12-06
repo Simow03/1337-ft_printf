80534d369830aa62ad9b88a9a355ea462856b470
mstaali <imthetigerz@gmail.com>
2023-12-06 23:13:25 +0100

    flags_modif

--- Bonus/ft_checkflags.c
+++ Bonus/ft_checkflags.c
@@ -6,36 +6,48 @@
 /*   By: mstaali <mstaali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:33:25 by mstaali           #+#    #+#             */
-/*   Updated: 2023/12/05 16:10:22 by mstaali          ###   ########.fr       */
+/*   Updated: 2023/12/06 23:07:28 by mstaali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-static int	ft_isHash(const char **str, va_list ptr)
+static int	ft_ishash(const char **str, va_list ptr)
 {
-	int len;
-
-	len = 0;
-	while (*(*str) == '#' || *(*str) == ' ' || *(*str) == '+')
-		(*str)++;
-	if (*(*str) == 'x')
-		len += write(1, "0x", 2) + ft_putnbr_hexalower(va_arg(ptr, int));
-	else if (*(*str) == 'X')
-		len += write(1, "0X", 2) + ft_putnbr_hexaupper(va_arg(ptr, int));
-	else
-		len += ft_checkconv(*(*str), ptr);
-	return (len);
-}
-
-static int	ft_isPlus(const char **str, va_list ptr)
-{
-	int len;
+	int	len;
 	int	value;
 
 	len = 0;
-	while (*(*str) == '+' || *(*str) == ' ' || *(*str) == '#')
-		(*str)++;
+	value = va_arg(ptr, int);
+	(*str)++;
+	if (value == 0)
+	{
+		if (*(*str) == 'x')
+			len += ft_putnbr_hexalower(value);
+		else if (*(*str) == 'X')
+			len += ft_putnbr_hexaupper(value);
+		else
+			len += ft_checkconv(*(*str), ptr);
+	}
+	else
+	{
+		if (*(*str) == 'x')
+			len += write(1, "0x", 2) + ft_putnbr_hexalower(value);
+		else if (*(*str) == 'X')
+			len += write(1, "0X", 2) + ft_putnbr_hexaupper(value);
+		else
+			len += ft_checkconv(*(*str), ptr);
+	}
+	return (len);
+}
+
+static int	ft_isplus(const char **str, va_list ptr)
+{
+	int	len;
+	int	value;
+
+	len = 0;
+	(*str)++;
 	if (*(*str) == 'd' || *(*str) == 'i')
 	{
 		value = va_arg(ptr, int);
@@ -49,14 +61,13 @@ static int	ft_isPlus(const char **str, va_list ptr)
 	return (len);
 }
 
-static int	ft_isSpace(const char **str, va_list ptr)
+static int	ft_isspace(const char **str, va_list ptr)
 {
-	int len;
+	int	len;
 	int	value;
 
 	len = 0;
-	while (*(*str) == ' ' || *(*str) == '+' || *(*str) == '#')
-		(*str)++;
+	(*str)++;
 	if (*(*str) == 'd' || *(*str) == 'i')
 	{
 		value = va_arg(ptr, int);
@@ -76,10 +87,10 @@ int	ft_checkflags(const char **str, va_list ptr)
 
 	len = 0;
 	if (*(*str) == '#')
-		len += ft_isHash(str, ptr);
+		len += ft_ishash(str, ptr);
 	else if (*(*str) == '+')
-		len += ft_isPlus(str, ptr);
+		len += ft_isplus(str, ptr);
 	else if (*(*str) == ' ')
-		len += ft_isSpace(str, ptr);
+		len += ft_isspace(str, ptr);
 	return (len);
 }
